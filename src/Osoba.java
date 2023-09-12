d8598f7fb400953e707b5d62a45153692e5566a6
taron8 <adamtaton92@gmail.com>
2023-09-12 11:18:15 +0200

    wlasnosci w klasie osoba prywatne,metody dostepowe

--- src/Osoba.java
+++ src/Osoba.java
@@ -1,5 +1,39 @@
 public class Osoba {
-    public String imie;
-    public String nazwisko;
-    public int wiek;
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        if(imie.length()<3)
+            System.out.println("Imie musi miec conajmniej 3 znaki");
+        else {
+            this.imie = imie;
+        }
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        if(wiek<0){
+            this.wiek = 0;
+        }
+        else{
+            this.wiek = wiek;
+        }
+
+    }
 }

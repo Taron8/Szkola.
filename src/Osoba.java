f8af58a9c65de7b1cbbb273ccba31963d6243dc6
taron8 <adamtaton92@gmail.com>
2023-09-12 11:44:39 +0200

    klasa abstrakcyjna

--- src/Osoba.java
+++ src/Osoba.java
@@ -1,4 +1,6 @@
-public class Osoba {
+public abstract class Osoba {
+    //klasa abstrakcyjna nie mozna utworzyc obiektu tej klasy
+    // klasa jest wykorzystywana jako klasa bazowa
     private String imie;
     private String nazwisko;
     private int wiek;

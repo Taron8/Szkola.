3769f269613a6c8030e56df3ced6b751075c5272
taron8 <adamtaton92@gmail.com>
2023-09-12 11:28:49 +0200

    konstruktory,przeciazanie,metoda toString

--- src/Osoba.java
+++ src/Osoba.java
@@ -3,6 +3,25 @@ public class Osoba {
     private String nazwisko;
     private int wiek;
 
+    public Osoba() {
+        this.imie = "Edward";
+        this.nazwisko = "Nowak";
+        this.wiek = 30;
+        //konstruktor z wartosciami domyslnymi
+    }
+
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = 7;
+    }
+
+    public Osoba(String imie, String nazwisko, int wiek) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
     public String getImie() {
         return imie;
     }
@@ -35,5 +54,14 @@ public class Osoba {
             this.wiek = wiek;
         }
 
+    }
+
+    @Override
+    public String toString() {
+        return "Osoba{" +
+                "imie='" + imie + '\'' +
+                ", nazwisko='" + nazwisko + '\'' +
+                ", wiek=" + wiek +
+                '}';
     }
 }

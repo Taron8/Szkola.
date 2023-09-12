9fae8328f76c6e83fa8f5e87091894e12dcef24d
taron8 <adamtaton92@gmail.com>
2023-09-12 12:35:41 +0200

    dodanie klasy w wychowawcy

--- src/Wychowawca.java
+++ src/Wychowawca.java
@@ -1,7 +1,9 @@
 public final class Wychowawca extends Nauczyciel{
   //final nie mozna z tej klasy dziedziczyc
+    private Klasa klasa;
 
-    public Wychowawca(String imie, String nazwisko) {
-        super(imie, nazwisko);
+    public Wychowawca(String imie, String nazwisko, Klasa klasa, String... przedmioty) {
+        super(imie, nazwisko, przedmioty);
+        this.klasa = klasa;
     }
 }
